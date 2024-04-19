e1e684f683c65b90c1a8b6c3268d23f46c426141
Mateus Felipe <brasil.mateus748@gmail.com>
2024-04-19 19:11:19 -0300

    Create constructor for prompting user to enter name and year of each student

--- src/StudentManagementSystem/Main.java
+++ src/StudentManagementSystem/Main.java
@@ -2,6 +2,6 @@ package StudentManagementSystem;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student student1 = new Student();
     }
 }
--- src/StudentManagementSystem/Student.java
+++ src/StudentManagementSystem/Student.java
@@ -1,4 +1,25 @@
 package StudentManagementSystem;
-public class Student {
+import java.util.Scanner;
 
+import static java.lang.System.in;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int gradeYear;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private int coursePrice = 700;
+
+    public Student() {
+        Scanner scanner = new Scanner(in);
+        System.out.println("Enter the first name of the student: ");
+        this.firstName = scanner.nextLine();
+        System.out.println("Enter the last name of the student: ");
+        this.lastName = scanner.nextLine();
+        System.out.println("1 - Freshman\n2 - Sophomore\n3 - Junior\n4 - Senior\n Enter the grade year of the student: ");
+        this.gradeYear = scanner.nextInt();
+        System.out.println(firstName + " " + lastName + " " + gradeYear);
+    }
 }
