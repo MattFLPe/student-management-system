5ae02d220c4d056bc617998147e65c0360a0c79f
Mateus Felipe <brasil.mateus748@gmail.com>
2024-06-21 12:07:16 -0300

    Implement for loop Implement logic to add a specific number of students

--- src/StudentManagementSystem/Main.java
+++ src/StudentManagementSystem/Main.java
@@ -1,8 +1,19 @@
 package StudentManagementSystem;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Student student1 = new Student();
-        student1.enroll();
+        System.out.println("Enter number of new students:");
+        Scanner in = new Scanner(System.in);
+        int numberOfStudents = in.nextInt();
+        Student[] students = new Student[numberOfStudents];
+
+        for (int n = 0; n < numberOfStudents; n++){
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+            System.out.println(students[n].toString());
+        }
     }
 }
